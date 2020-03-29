ecfd0de5950dd7abc4051b361b028e2e50be67f9
Diwesh Singh <diwesh.singh6@gmail.com>
2020-03-29 14:44:32 +0530

    Adding to remote repo

---
+++ app/src/main/java/info/spreadcode/bottomnavigationview/BottomNavigationActivity.kt
@@ -0,0 +1,89 @@
+package info.spreadcode.bottomnavigationview
+
+import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_bottom_navigation.*
+
+class BottomNavigationActivity : AppCompatActivity(),
+    BottomNavigationView.OnNavigationItemSelectedListener {
+
+    private val homeFragment = HomeFragment()
+    private val trendingFragment = TrendingFragment()
+    private val favouriteFragment = FavouriteFragment()
+
+    private lateinit var activeFragment: Fragment
+
+    private val fragmentManager = supportFragmentManager
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bottom_navigation)
+
+        // Set up initial value to views and add views to listener
+        setUpViewsAndListeners()
+
+        // Set up initial fragment transaction
+        setUpFragmentTransaction()
+
+
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        supportActionBar!!.title = item.title
+        when (item.itemId) {
+            R.id.home_navigation -> {
+                fragmentManager.beginTransaction().hide(activeFragment).show(homeFragment).commit()
+                activeFragment = homeFragment
+                return true
+            }
+
+            R.id.trending_navigation -> {
+                fragmentManager.beginTransaction().hide(activeFragment).show(trendingFragment)
+                    .commit()
+                activeFragment = trendingFragment
+                return true
+            }
+            R.id.favourite_navigation -> {
+                fragmentManager.beginTransaction().hide(activeFragment).show(favouriteFragment)
+                    .commit()
+                activeFragment = favouriteFragment
+                return true
+            }
+
+        }
+        return false
+    }
+
+    /**
+     * Set up initial value to views and attach views to listener
+     */
+    private fun setUpViewsAndListeners() {
+
+        // Assign first fragment to the active fragment
+        activeFragment = homeFragment
+
+        // Attach bottom navigation view to listener
+        bottom_navigation_view.setOnNavigationItemSelectedListener(this)
+
+        // set first fragment title to the toolbar title
+        supportActionBar!!.title = getString(R.string.home_fragment_bottom_navigation_title)
+    }
+
+    /**
+     * Attach fragment to the FragmentManager, hide and commit
+     */
+    private fun setUpFragmentTransaction() {
+
+        fragmentManager.beginTransaction().add(R.id.fragment_container, favouriteFragment, "3")
+            .hide(favouriteFragment).commit()
+
+        fragmentManager.beginTransaction().add(R.id.fragment_container, trendingFragment, "2")
+            .hide(trendingFragment).commit()
+
+        fragmentManager.beginTransaction().add(R.id.fragment_container, homeFragment, "1").commit()
+    }
+
+}
